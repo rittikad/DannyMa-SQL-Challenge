c4caf4d07f4de0fb9a35c7ba7c224dbf4c7b5a2e
Rittika Dolui <66118150+rittikad@users.noreply.github.com>
2025-11-03 16:38:47 +0000

    Add files via upload

---
+++ Case Study #2 - Pizza Runner/pizza_runner_sql.sql
@@ -0,0 +1,9 @@
+USE pizza_runner;
+SELECT * FROM runner_orders;
+SELECT * FROM customer_orders;
+SELECT * FROM pizza_names;
+SELECT * FROM pizza_recipes;
+SELECT * FROM pizza_toppings;
+SELECT * FROM runners;
+
+DELETE FROM customer_orders;
